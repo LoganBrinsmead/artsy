fe58515f280670a69e62ef87ed9655dbc9ce02b9
loganbrinsmead <loganbrinsmead1115@gmail.com>
2025-10-25 15:36:31 -0700

    work

--- api/MET.js
+++ api/MET.js
@@ -18,6 +18,10 @@ class MetAPI {
         this.searchDataCache = {}
     }
 
+    /*
+        Returns object ID's for items that contain keywords in their objects
+        to get images, use this in conjunction with Object method
+    */
     async search(searchTerm) {
         if (this.searchDataCache[searchTerm]) return this.searchDataCache[searchTerm];
 
@@ -30,6 +34,18 @@ class MetAPI {
         return this.searchDataCache;
 
     }
+
+    /*
+        Returns images, description, and more according to ID for object
+        see documentation on website for more information
+    */
+   async getObject(objectId) {
+        let objectFragment = `/public/collection/v1/objects/${objectID}`;
+
+        let data = await fetch(this.baseUrl + objectFragment);
+
+        return await data.json();
+   }
 }
 
 const metAPI = new MetAPI(metBaseURL);
