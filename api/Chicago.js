cfe2c2c96949918e781df8e277ed1e97f165b6ee
loganbrinsmead <loganbrinsmead1115@gmail.com>
2025-10-29 02:20:18 -0700

    progress

--- api/Chicago.js
+++ api/Chicago.js
@@ -4,8 +4,12 @@ export default class Chicago {
         this.name= "Art Institute of Chicago";
         this.baseURL = "https://api.artic.edu/api/v1/"
         this.objectsBySearchCache = {};
+        this.iifURL = "https://www.artic.edu/iiif/2";
     }
 
+    // TODO: turn this into search function
+    //  include image URL in the data for parsing
+    // parse data out so it is extendable, create a parse function to do this.
     async getObjectsBySearch(searchTerm) {
         if(this.objectsBySearchCache[searchTerm]) return this.objectsBySearchCache[searchTerm];
 
@@ -18,5 +22,9 @@ export default class Chicago {
         return this.objectsBySearchCache[searchTerm];
     }
 
+    getImageByID(identifier) {
+        return `https://www.artic.edu/iiif/2/${identifier}/full/843,/0/default.jpg`;
+
+    }
 
 }
